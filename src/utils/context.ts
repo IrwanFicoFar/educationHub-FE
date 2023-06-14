9f4e162aedaf9b5bf0bdac25dd4f3dc8debd6520
Irwan Hadi <41413060+IrwanFicoFar@users.noreply.github.com>
2023-06-14 21:03:40 +0800

    Update context.ts

--- src/utils/context.ts
+++ src/utils/context.ts
@@ -6,20 +6,10 @@ interface ThemeType {
   setTheme: React.Dispatch<React.SetStateAction<string>>;
 }
 
-interface ToggleType {
-  toggle: string;
-  setToggle: React.Dispatch<React.SetStateAction<string>>;
-}
-
 const theme: ThemeType = {
   theme: "",
   setTheme: () => {},
 };
 
-const toggle: ToggleType = {
-  toggle: "",
-  setToggle: () => {},
-};
-
 export const ThemeContext = createContext(theme);
-export const ToggleContext = createContext(toggle);
+
